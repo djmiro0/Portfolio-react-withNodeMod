c8a60f6fbbaf78949ea8885b2d9d643bc0613f7d
Djordje Mirosavljevic <djordje.mirosavljevic@dci.education>
2023-04-24 11:00:24 +0200

    aboutme

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import "../styles/AboutMe.css";
 
 export default function AboutMe() {
-  return <div>AboutMe</div>;
+  const { t } = useTranslation();
+  return (
+    <div className="container">
+      <div className="programs"></div>
+      <div className="aboutMeContainer">
+        <div className="img"></div>
+        <p className="aboutMeText">{t("aboutMeText")}</p>
+      </div>
+    </div>
+  );
 }
